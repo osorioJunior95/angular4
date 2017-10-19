d20316a8b41d0d84670aa3878566894f87093515
Junior Oliveira <osorio.junior95@gmail.com>
2017-10-19 21:09:39 -0200

    Sudinbo mais alterações

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,22 +10,25 @@ import { HEROES } from './const';
   <h1>{{title}}</h1>
   <h2>My Heroes</h2>
   <ul class="heroes">
-    <li *ngFor="let hero of heroes">
+    <li *ngFor="let hero of heroes" (click)="onSelect(hero)" [class.selected]="hero === selectedHero">
       <span class="badge">{{hero.id}}</span>{{hero.name}}
     </li>
   </ul>
-	<h2>{{hero.name}} details!</h2>
-	<div><label>id: </label>{{hero.id}}</div>
-	<div>
-		<label>name: </label>
-		<input [(ngModel)]="hero.name" placeholder="name">
-	</div>`
+  <div *ngIf="selectedHero">
+    <h2>{{selectedHero.name}} details!</h2>
+    <div><label>id: </label>{{selectedHero.id}}</div>
+    <div>
+      <label>name: </label>
+      <input [(ngModel)]="selectedHero.name" placeholder="name">
+    </div>
+  </div>`
 })
 
 export class AppComponent { 
-  hero = HEROES;
+  heroes = HEROES;
+  selectedHero: Hero;
 
-  constructor() {
-    console.log(this.hero)            
+  onSelect(hero: Hero): void{
+    this.selectedHero = hero;
   }
 }
